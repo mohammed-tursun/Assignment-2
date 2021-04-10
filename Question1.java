27f32eb75b605a5f7570912007680d28ca8f6c33
mohammed-tursun <81074656+mohammed-tursun@users.noreply.github.com>
2021-04-10 11:03:11 +0200

    Assignment _2Tasks

---
+++ Question1.java
@@ -0,0 +1,88 @@
+package assignment_2;
+
+public class Question1 {
+
+	private static String java;
+	
+
+	public static void main(String[] args) {
+		
+		//QUESTION 1
+		int count = 100_000;
+		System.out.println("number is "+ count);
+		
+		
+		//Question 2
+		String message = "I can do it " ;
+		System.out.println("can you do it? "+ message);
+		
+		
+		//Question 3 
+		int num;double cost;
+		num = 12;
+		cost = 15;
+		System.out.println("flightnumber " +  num   +" number for cost "+ cost);
+		
+
+		//Question 4
+        
+		String example ;
+	    example ="Java"; 
+		System.out.println("Today's Word-Of-The-Day is:"+example);	
+		
+		//Question5
+		
+		String first = "first is 55 ";
+		float second = 123f;
+		System.out.println(first + "second=" + second  );
+		
+
+		//Question6
+		
+		String Bop , Jill , Betty , Herb ,James;
+		Bop = "bop ,";
+		Jill= "JIll ,";
+		Betty= "betty, ";
+		Herb = "Herb ,";
+		James = "James ,";
+		System.out.println(Bop + Jill + Betty + Herb + James);
+		
+		//Question7
+		
+		int i = 9; float f =33.4f;
+		System.out.println("i= "+i+" "+"f= "+f);
+		
+		//QUESTION8 
+		String lan1 = "\"java\"and";
+		String lan2 = "\"SQL\"at CybertekSchool.";
+		System.out.println("I will Learn" +lan1 +  lan2);
+		
+		//Question9
+		
+		byte steps = 100;
+		short miles = 5000;
+		int Count = 1000000;
+		long population =3434455667l;
+		System.out.println(steps );
+		System.out.println(miles);
+		System.out.println(Count);
+		System.out.println(population);
+		
+		//Question 10
+		//i already use the word of second so i add 2
+		int hour,minute,second2;
+		String morning ,noon;
+		hour = 11;
+		minute = 32;
+		second2 = 55;
+		morning = " AM ";
+		noon = " PM ";
+		
+		System.out.println(hour+":"+ minute+":"+second2 + morning);
+		
+		
+		
+
+	}
+
+}
